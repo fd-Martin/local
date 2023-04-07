f8575778ec9a2738ce5e105fde9e7f628a3ac8f6
fd-Martin <fdmosharof@gmail.com>
2023-04-07 16:11:44 +0600

    stored

--- src/Utilities/fakedb.jsx
+++ src/Utilities/fakedb.jsx
@@ -1,25 +1,24 @@
 const addToDb = id => {
 
     let shoppingCart;
-
-    const storedCart=localStorage.getItem('shopping-cart');
-    if(storedCart){
-        shoppingCart= JSON.parse(storedCart);
-    }else{
-        shoppingCart={};
+    //get the shopping cart
+    const storedCart = localStorage.getItem('shopping-cart');
+    if (storedCart) {
+        shoppingCart = JSON.parse(storedCart);
+    } else {
+        shoppingCart = {};
     }
 
 
     //add   quantity
     const quantity = shoppingCart[id];
     if (quantity) {
-        // console.log('Already exits');
+
         const newQuantity = parseInt(quantity) + 1;
         shoppingCart[id] = newQuantity;
-        // localStorage.setItem(id,newQuantity);
+
     } else {
-        // console.log('new item');
-        // localStorage.setItem(id,1);
+
         shoppingCart[id] = 1;
     }
     localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
